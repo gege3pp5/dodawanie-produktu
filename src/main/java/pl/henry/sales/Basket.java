ea659c10121c59271386acd8940bba650cb54787
Grzesiek <wiecek.grzegorz@op.pl>
2017-12-17 18:34:50 +0000

    Added basket

--- src/main/java/pl/henry/sales/Basket.java
+++ src/main/java/pl/henry/sales/Basket.java
@@ -1,5 +1,36 @@
 package pl.henry.sales;
 
-public class Basket {
+import java.util.HashMap;
 
+public class Basket {
+  private HashMap<String, Integer> productQuantities;
+  private Stock stock;
+  
+  public Basket(Stock stock) {
+    this.stock = stock;
+    productQuantities = new HashMap<>();
+  }
+
+  public int put(String productName, int quantity) throws ProductNotAvailableException {
+    if(quantity > stock.getQuantity(productName))
+      throw new ProductNotAvailableException();
+    int newQuantity = getQuantity(productName) + quantity;
+    if(newQuantity < 0)
+      newQuantity = 0;      
+    productQuantities.put(productName, newQuantity);
+    return newQuantity;
+  }
+
+  public void clear() {
+    productQuantities = new HashMap<>();
+  }
+
+  public HashMap<String, Integer> getProductQuantities() {
+    return (HashMap<String, Integer>) productQuantities.clone();
+  }
+
+  public int getQuantity(String productName) {
+    Integer quantity = productQuantities.get(productName);
+    return quantity == null ? 0 : quantity;
+  }
 }
