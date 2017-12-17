015b7b17ecd0e15e096c8c3319eaf2ab0eb756aa
Grzesiek <wiecek.grzegorz@op.pl>
2017-12-17 12:50:45 +0000

    Added simple Product class

---
+++ src/main/java/pl/henry/sales/Product.java
@@ -0,0 +1,13 @@
+package pl.henry.sales;
+
+public class Product {
+  private String name;
+
+  public Product(String name) {
+    this.name = name;
+  }
+
+  public String toString() {
+    return name;
+  }
+}
